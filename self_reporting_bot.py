435150b52380ef63cfc02e3521a440dc64a550a0
Hocuri <hocuri@gmx.de>
2025-04-15 16:56:11 +0200

    Put the statistics into the attachment

--- self_reporting_bot.py
+++ self_reporting_bot.py
@@ -1,9 +1,11 @@
 import logging
 import os.path
 from pathlib import Path
+import json
 
 from deltabot_cli import BotCli
 from deltachat2 import EventType, events
+from deltachat2.types import SpecialContactId
 from rich.logging import RichHandler
 
 cli = BotCli("self_reporting_bot")
@@ -14,32 +16,26 @@ logging.basicConfig(
 )
 
 
-@cli.on(events.RawEvent)
-def log_event(bot, accid, event):
-    if event.kind == EventType.INFO:
-        bot.logger.info(event.msg)
-    elif event.kind == EventType.WARNING:
-        bot.logger.warning(event.msg)
-    elif event.kind == EventType.ERROR:
-        bot.logger.error(event.msg)
-
-
 @cli.on(events.NewMessage)
 def on_new_message(bot, accid, event):
     chatid = event.msg.chat_id
+    msg = event.msg
     try:
-        text = event.msg.text
-        bot.rpc.delete_messages(accid, [event.msg.id])
+        if msg.text.startswith("core_version "):
+            bot.rpc.misc_send_text_message(
+                accid,
+                chatid,
+                "Thanks for sending statistics about your usage of Delta Chat to us!",  # TODO
+            )
+            return
 
-        if not text.startswith("core_version "):
-            raise ValueError("Message doesn't start with core_version")
+        if msg.file_name != "statistics.txt":
+            raise ValueError("Message doesn't contain statistics.txt")
 
-        statistics = {}
-        for l in text.splitlines():
-            parts = l.split()
-            statistics[parts[0]] = parts[1]
+        with open(msg.file) as file:
+            new_data = json.load(file)
 
-        self_reporting_id = statistics["self_reporting_id"]
+        self_reporting_id = new_data["self_reporting_id"]
         if any(c not in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_" for c in self_reporting_id):
             raise ValueError("Invalid self_reporting_id")
         if len(self_reporting_id) < 11 or len(self_reporting_id) > 32:
@@ -47,13 +43,16 @@ def on_new_message(bot, accid, event):
 
         Path("reports").mkdir(exist_ok=True)
         filename = os.path.join("reports", self_reporting_id)
-        version = 1
-        while os.path.exists(filename + "." + str(version)):
-            version += 1
-        filename = filename + "." + str(version)
+
+        if os.path.exists(filename):
+            with open(filename) as file:
+                existing_data = json.load(file)
+        else:
+            existing_data = []
+        existing_data.append(new_data)
 
         with open(filename, "w") as file:
-            file.write(text)
+            json.dump(existing_data, file, indent=4)
 
         bot.rpc.misc_send_text_message(
             accid,
@@ -68,7 +67,25 @@ def on_new_message(bot, accid, event):
             "Sorry, I couldn't understand your message.\n\nI am a bot for receiving statistics about your usage of Delta Chat. All other messages will be ignored.",
         )
 
-# TODO: On message delivered, delete chat & contact
+
+@cli.on(events.RawEvent)
+def log_event(bot, accid, event):
+    if event.kind == EventType.INFO:
+        bot.logger.info(event.msg)
+    elif event.kind == EventType.WARNING:
+        bot.logger.warning(event.msg)
+    elif event.kind == EventType.ERROR:
+        bot.logger.error(event.msg)
+    elif event.kind == EventType.MSG_DELIVERED:
+        delete_everything(bot, accid, bot.rpc.get_message(accid, event.msg_id))
+
+def delete_everything(bot, accid, msg):
+    contact_ids = bot.rpc.get_chat_contacts(accid, msg.chat_id)
+    bot.rpc.delete_chat(accid, msg.chat_id)
+    for contact_id in contact_ids:
+        if contact_id != SpecialContactId.SELF:
+            bot.rpc.delete_contact(accid, contact_ids[0])
+
 
 def main():
     cli.start()
@@ -76,3 +93,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+@cli.on_init
+def on_init(bot, args):
+    bot.logger.info("Initializing CLI with args: %s", args)
+    for accid in bot.rpc.get_all_account_ids():
+        bot.rpc.set_config(accid, "delete_server_after", "1")
+        bot.rpc.set_config(accid, "delete_device_after", "3600")
